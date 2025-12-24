78ec0f6006ff445782b070f0fdba0fbf8a468ac4
Miguel Acevedo <39278733+AwesomeVedo@users.noreply.github.com>
2025-12-24 02:51:09 -0500

    Build todo app with state, persistence, and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [list, setList] = useState<string[]>(() => {
+    const saved = localStorage.getItem("list")
+    return saved ? JSON.parse(saved) : []
+  })
+  
+  const [text, setText] = useState("");
+
+
+  useEffect(() => { 
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list]) 
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <h1>Playground</h1>
+       <h2>Todo List</h2>
+       <input 
+          placeholder="Add a todo" 
+          value={text}
+          onChange={(e) => setText(e.target.value) }
+       />
+      <button onClick={
+        () => {
+          if (text.trim()) {
+            setList([...list, text]);
+          }
+        }
+      }>Add To List</button>
+      <button onClick={
+        () => {
+          console.log(list)
+        }
+      }>Console Test</button>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}
+          <button onClick={
+            () => {
+              const editedList = list.filter((_, i) => i !== index)
+              setList(editedList)
+            }
+          }>Delete</button>
+          </li>
+          
+        ))}
+      </ul>
     </>
   )
 }
